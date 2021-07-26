5c7bbb9af0c08dae26ea0edd6e9be683dc5f0c83
Arturo <arturo.soga01@gmail.com>
2021-07-26 09:33:00 -0500

    base

---
+++ SoapService/IMiSoap1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SoapService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IMiSoap1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IMiSoap1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        public string StringValue
+        {
+            get { return StringValue; }
+            set { StringValue = value; }
+        }
+    }
+}
---
+++ SoapService/MiSoap1.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SoapService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "MiSoap1" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione MiSoap1.svc o MiSoap1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class MiSoap1 : IMiSoap1
+    {
+       public string GetData(int value)
+        {
+            return string.Format("you entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if(composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
